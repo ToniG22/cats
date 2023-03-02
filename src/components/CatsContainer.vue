5962778c3475fe14dde60bec395124f6993004f6
ToniG22 <tonigarces19@gmail.com>
2023-03-02 22:58:57 +0000

    first commit

---
+++ src/components/CatsContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <CatsView :fetchedData="fetchedCats"></CatsView>
+</template>
+
+<script>
+import CatsView from '../views/CatsView.vue'
+const catsUrl = 'https://api.thecatapi.com/v1/images/search?size=full'
+const apiKey = 'live_tAan3X2z0vxtNNgb5bsXo0D2B14n9HkCgdFk8X8MGUrPKInUUImYgCfEwxJlAQPr'
+
+export default {
+    data() {
+        return {
+            fetchedCats: []
+        }
+    },
+    components: {
+        CatsView
+    },
+    async created() {
+        //Get cat images from cats API
+        this.fetchedCats = await fetch(catsUrl, {headers: {'x-api-key': apiKey}}, {mode: 'no-cors'}).then(r => r.json())
+    }
+}
+
+</script>
---
+++ src/views/CatsView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="cats">
+        <button>Click</button>
+        <img v-for="cat in fetchedData" :src="cat.url">
+        <h1>This is an cats page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            fetchedData: Array
+        }
+    }
+</script>
+
+<style scoped>
+.cats {
+    display: grid;
+    gap: 100px;
+    grid-template-columns: repeat(2, 1fr);
+    justify-items: center;
+    align-items: center;
+}
+
+img {
+    width: 350px;
+    aspect-ratio: 16/9;
+    border-radius: .5rem;
+}
+</style>
